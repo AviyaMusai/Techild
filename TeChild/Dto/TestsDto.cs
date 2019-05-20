4e7e5070b06c540cf0bba9267996fe8271d54d42
AviyaMusai <234aviya@gmail.com>
2019-05-20 23:37:09 +0300

    ok

---
+++ TeChild/Dto/TestsDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace Dto
+{
+  public  class TestsDto
+    {
+        public int id { get; set; }
+        public int lessonAndSubjectId { get; set; }
+        public string testFile { get; set; }
+
+     //   public virtual Lessons Lessons { get; set; }
+     public static TestsDto ConvertDBToDto(Tests tests)
+        {
+            return new TestsDto()
+            {
+                id = tests.id,
+                lessonAndSubjectId = tests.lessonAndSubjectId,
+                testFile = tests.testFile
+            };
+        }
+    }
+}
