44564929d36b295ab32d6997764cbd835c5c463c
AviyaMusai <234aviya@gmail.com>
2019-05-20 23:51:18 +0300

    ok

---
+++ TeChild/BLL/TestsLogic.cs
@@ -0,0 +1,24 @@
+﻿using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+  public  class TestsLogic
+    {
+        public static DAL.TechildDBEntities db = new DAL.TechildDBEntities();
+        public static List<TestsDto> GetAllTest()
+        {
+            List<TestsDto> t = new List<TestsDto>();
+            var tests = db.Tests.ToList();
+            foreach (var item in tests)
+            {
+                t.Add(Dto.TestsDto.ConvertDBToDto(item));
+            }
+            return t;
+        }
+    }
+}
