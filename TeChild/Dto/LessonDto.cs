b6215af413b2fbe7c7019713e60310385155c2fb
AviyaMusai <234aviya@gmail.com>
2019-05-21 15:35:40 +0300

    ok

---
+++ TeChild/Dto/LessonDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+  public  class LessonDto
+    {
+        public int subjectId { get; set; }
+        public int lessonId { get; set; }
+        public string lessonName { get; set; }
+        public string lessonFile { get; set; }
+        public int id { get; set; }
+
+        //public virtual Subjects Subjects { get; set; }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<Progress> Progress { get; set; }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<Tests> Tests { get; set; }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<Users> Users { get; set; }
+    }
+}
