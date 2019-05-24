d2b7b05dc2bf0b4fb94c2a34792e045abc1a1502
AviyaMusai <234aviya@gmail.com>
2019-05-24 17:32:38 +0300

    24/05/19

---
+++ TeChild/BLL/ProgressLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dto;
+namespace BLL
+{
+  public class ProgressLogic
+    {
+        public static DAL.TechildDBEntities db = new DAL.TechildDBEntities();
+        //החזרת כל ההתקדמויות של משתמש לפי קוד משתמש
+        public static List<ProgressDto> GetProgressByUserId(int userId)
+        {
+            List<ProgressDto> p = new List<ProgressDto>();
+            var progress = from pro in db.Progress
+                           where pro.userId == userId
+                           select pro;
+            foreach (var item in progress)
+            {
+                p.Add(Dto.ProgressDto.ConvertDBToDto(item));
+            }
+            return p;
+        }
+        //החזרת ההתקדמויות של נושא מסוים למשתמש מסוים לפי קוד נושא וקוד משתמש
+        public static List<ProgressDto> GetProgressByUserIdAndSubjectid(int userId,int subId)
+        {
+            List<ProgressDto> p = new List<ProgressDto>();
+            var progress = from pro in db.Progress
+                           where pro.userId == userId&&
+                           pro.lessonAndSubjectId.Equals(subId)
+                           select pro;
+            foreach (var item in progress)
+            {
+                p.Add(Dto.ProgressDto.ConvertDBToDto(item));
+            }
+            return p;
+        }
+    }
+}
---
+++ TeChild/Dto/ProgressDto.cs
@@ -0,0 +1,32 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+  public  class ProgressDto
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public int lessonAndSubjectId { get; set; }
+        public int isPass { get; set; }
+        public int mark { get; set; }
+
+        //public virtual Lessons Lessons { get; set; }
+        //public virtual Users Users { get; set; }
+        public static ProgressDto ConvertDBToDto(Progress progress)
+        {
+            return new ProgressDto
+            {
+                id = progress.id,
+                userId = progress.userId,
+                lessonAndSubjectId = progress.lessonAndSubjectId,
+                isPass = progress.isPass,
+                mark = progress.mark
+            };
+        }
+    }
+}
